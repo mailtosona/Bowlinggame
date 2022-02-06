eaf7abd37c7a04a776d1c3510dcb62e46141c469
mailtosona <mailtosona@ymail.com>
2022-02-06 02:48:04 +0000

    testing case changes adding more cases

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,4 +1,53 @@
-import static org.junit.jupiter.api.Assertions.*;
-class BowlingGameTest {
-  
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BowlingGameTest {
+    private int score=0;
+    public BowlingGame game=new BowlingGame();
+@Test
+    public void rollingOneScoresOne() throws Exception
+{
+
+    game.roll(1);
+    assertEquals(1,game.score());
+}
+    @Test
+    public void roliingTwoScoresTwo() throws Exception
+{
+    game.roll(2);
+    assertEquals(2,game.score());
+}
+    @Test
+    public void rollingASpareThanOneScoresTwo() throws Exception
+    {
+        for (int i=0;i<2;i++)
+        game.roll(1);
+        assertEquals(2,game.score());
+    }
+    @Test
+    public void rollingASpareThanOneScoresTwelve() throws Exception
+    {
+   game.roll(5);
+   game.roll(5);
+   game.roll(1);
+   assertEquals(12,game.score());
+    }
+    @Test
+    public void rollingASpareThanOneScoresThirteen() throws Exception
+    {
+        game.roll(1);
+        game.roll(1);
+        game.roll(5);
+        game.roll(5);
+        game.roll(1);
+        assertEquals(14,game.score());
+    }
+    protected void rollspare()
+    {
+        game.roll(5);
+        game.roll(5);
+    }
+
 }
